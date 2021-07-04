d07eb66d011cb36e2e499e71e4a9820dc168e007
ahctim <mitch@mitchya1.com>
2021-07-04 07:42:40 -0600

    add new members

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -5,10 +5,12 @@ package types
 // Ideally we'd use something provided by the SDK
 type AlarmEvent struct {
 	AlarmName        string `json:"AlarmName"`
+	AlarmSource      string `json:"AlarmSource"`
 	AlarmDescription string `json:"AlarmDescription"` // This may be null
 	Region           string `json:"Region"`
 	NewStateValue    string `json:"NewStateValue"`
 	NewStateReason   string `json:"NewStateReason"`
+	AWSAccountId     string `json:"AWSAccountId"` // If this exists, we assume the message is from Cloudwatch
 	ID               string
 	Created          string
 }
